9fb7ab4ac9b926f7f99bb25abdc0a7b299ce93ff
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2024-01-10 21:02:33 +0300

    Delete js_SendAndHandlerPostRequest directory

--- js_SendAndHandlerPostRequest/Program.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
-var app = builder.Build();
-app.UseStaticFiles();
-app.MapControllerRoute("default","{controller=Home}/{action=Index}");
-
-app.Run();
